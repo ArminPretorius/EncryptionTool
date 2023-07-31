ecf2c72f5fac169f46619ec07a2b708878c0e2fa
Armin Pretorius <arminp1139@gmail.com>
2023-07-31 22:21:48 +0200

    Cloned Program to Repository

---
+++ CMPG215 program v2.0/CMPG215/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿
+namespace CMPG215
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btnEncrypt = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.txtPass = new System.Windows.Forms.TextBox();
+            this.btnDecrypt = new System.Windows.Forms.Button();
+            this.lblPass = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.cbFileType = new System.Windows.Forms.ComboBox();
+            this.lblType = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // btnEncrypt
+            // 
+            this.btnEncrypt.Location = new System.Drawing.Point(13, 325);
+            this.btnEncrypt.Name = "btnEncrypt";
+            this.btnEncrypt.Size = new System.Drawing.Size(244, 23);
+            this.btnEncrypt.TabIndex = 2;
+            this.btnEncrypt.Text = "Encrypt";
+            this.btnEncrypt.UseVisualStyleBackColor = true;
+            this.btnEncrypt.Click += new System.EventHandler(this.btnEncrypt_Click);
+            // 
+            // txtPass
+            // 
+            this.txtPass.Location = new System.Drawing.Point(88, 299);
+            this.txtPass.Name = "txtPass";
+            this.txtPass.PasswordChar = '*';
+            this.txtPass.Size = new System.Drawing.Size(154, 20);
+            this.txtPass.TabIndex = 1;
+            this.txtPass.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnDecrypt
+            // 
+            this.btnDecrypt.Location = new System.Drawing.Point(13, 440);
+            this.btnDecrypt.Name = "btnDecrypt";
+            this.btnDecrypt.Size = new System.Drawing.Size(244, 23);
+            this.btnDecrypt.TabIndex = 3;
+            this.btnDecrypt.Text = "Decrypt";
+            this.btnDecrypt.UseVisualStyleBackColor = true;
+            this.btnDecrypt.Click += new System.EventHandler(this.btnDecrypt_Click);
+            // 
+            // lblPass
+            // 
+            this.lblPass.AutoSize = true;
+            this.lblPass.Location = new System.Drawing.Point(26, 302);
+            this.lblPass.Name = "lblPass";
+            this.lblPass.Size = new System.Drawing.Size(56, 13);
+            this.lblPass.TabIndex = 3;
+            this.lblPass.Text = "Password:";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::CMPG215.Properties.Resources.Logo___Copy;
+            this.pictureBox1.Location = new System.Drawing.Point(13, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(244, 228);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPassword.Location = new System.Drawing.Point(26, 276);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(217, 16);
+            this.lblPassword.TabIndex = 5;
+            this.lblPassword.Text = "Please enter your password below:";
+            // 
+            // cbFileType
+            // 
+            this.cbFileType.FormattingEnabled = true;
+            this.cbFileType.Items.AddRange(new object[] {
+            "Picture (JPEG)",
+            "Text File",
+            "RAR File"});
+            this.cbFileType.Location = new System.Drawing.Point(76, 413);
+            this.cbFileType.Name = "cbFileType";
+            this.cbFileType.Size = new System.Drawing.Size(121, 21);
+            this.cbFileType.TabIndex = 6;
+            // 
+            // lblType
+            // 
+            this.lblType.AutoSize = true;
+            this.lblType.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblType.Location = new System.Drawing.Point(26, 394);
+            this.lblType.Name = "lblType";
+            this.lblType.Size = new System.Drawing.Size(217, 16);
+            this.lblType.TabIndex = 7;
+            this.lblType.Text = "Select a file type from the list below:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(65, 244);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(142, 24);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "ENCRYPTION";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(65, 364);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(141, 24);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "DECRYPTION";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(273, 471);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblType);
+            this.Controls.Add(this.cbFileType);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lblPass);
+            this.Controls.Add(this.btnDecrypt);
+            this.Controls.Add(this.txtPass);
+            this.Controls.Add(this.btnEncrypt);
+            this.Name = "Form1";
+            this.Text = "Group 5 - Encryption Software";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btnEncrypt;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.TextBox txtPass;
+        private System.Windows.Forms.Button btnDecrypt;
+        private System.Windows.Forms.Label lblPass;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.ComboBox cbFileType;
+        private System.Windows.Forms.Label lblType;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ CMPG215 program v2.0/CMPG215/Form1.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography; //Added
+using System.IO;                    //Added
+
+namespace CMPG215
+{
+    public partial class Form1 : Form
+    {
+        string ext;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //Encrypt Button
+        private void btnEncrypt_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            saveFileDialog1.ShowDialog();
+            Encrypt(openFileDialog1.FileName, saveFileDialog1.FileName, txtPass.Text);
+            ext = Path.GetExtension(openFileDialog1.FileName);
+        }
+        //GenerateSalt
+        public static byte[] GenSalt()
+        {
+            byte[] data = new byte[32];
+            using (RNGCryptoServiceProvider rgnCryptoServiceProvider = new RNGCryptoServiceProvider())
+            {
+                rgnCryptoServiceProvider.GetBytes(data);
+            }
+            return data;
+        }
+        //Encryption
+        private void Encrypt(string inputFile, string outputFile, string password)
+        {
+            byte[] salt = GenSalt();
+            byte[] passwords = Encoding.UTF8.GetBytes(password);
+            RijndaelManaged AES = new RijndaelManaged();
+            AES.KeySize = 256;
+            AES.BlockSize = 128;
+            AES.Padding = PaddingMode.PKCS7;
+            var key = new Rfc2898DeriveBytes(passwords, salt, 50000);
+            AES.Key = key.GetBytes(AES.KeySize / 8);
+            AES.IV = key.GetBytes(AES.BlockSize / 8);
+            AES.Mode = CipherMode.CFB;
+            using (FileStream fsCrypt = new FileStream(outputFile + ".aes", FileMode.Create))
+            {
+                fsCrypt.Write(salt, 0, salt.Length);
+                using (CryptoStream cs = new CryptoStream(fsCrypt, AES.CreateEncryptor(), CryptoStreamMode.Write))
+                {
+                    using (FileStream fsIn = new FileStream(inputFile, FileMode.Open))
+                    {
+                        byte[] buffer = new byte[1048576];
+                        int read;
+                        while ((read = fsIn.Read(buffer, 0, buffer.Length)) > 0)
+                        {
+                            cs.Write(buffer, 0, read);
+                        }
+                    }
+                }
+            }
+            txtPass.Clear();
+            File.Delete(inputFile); //delete unencrypted file
+            MessageBox.Show("File successfully encrypted!");
+        }
+        //Decryption
+        private void Decrypt(string inputFile, string outputFile, string password)
+        {
+            byte[] passwords = Encoding.UTF8.GetBytes(password);
+            byte[] salt = new byte[32];
+            using (FileStream fsCrypt = new FileStream(inputFile, FileMode.Open))
+            {
+                fsCrypt.Read(salt, 0, salt.Length);
+                RijndaelManaged AES = new RijndaelManaged();
+                AES.KeySize = 256;
+                AES.BlockSize = 128;
+                var key = new Rfc2898DeriveBytes(passwords, salt, 50000);
+                AES.Key = key.GetBytes(AES.KeySize / 8);
+                AES.IV = key.GetBytes(AES.BlockSize / 8);
+                AES.Padding = PaddingMode.PKCS7;
+                AES.Mode = CipherMode.CFB;
+                using (CryptoStream cryptoStream = new CryptoStream(fsCrypt, AES.CreateDecryptor(), CryptoStreamMode.Read))
+                {
+                    //save to file with orginal extension
+                    if (cbFileType.SelectedIndex == 0)
+                    {
+                        using (FileStream fsOut = new FileStream(outputFile + ".jpg", FileMode.Create))
+                        {
+                            try
+                            {
+                                int read;
+                                byte[] buffer = new byte[1048576];
+                                while ((read = cryptoStream.Read(buffer, 0, buffer.Length)) > 0)
+                                {
+                                    fsOut.Write(buffer, 0, read);
+                                }
+                                txtPass.Clear();
+                                MessageBox.Show("File successfully decrypted!");
+                            }
+                            catch
+                            {
+                                MessageBox.Show("Wrong Password!");
+                                this.Close();
+                            }
+                        }
+                    }
+                    else if (cbFileType.SelectedIndex == 1)
+                    {
+                        using (FileStream fsOut = new FileStream(outputFile + ".txt", FileMode.Create))
+                        {
+                            try
+                            {
+                                int read;
+                                byte[] buffer = new byte[1048576];
+                                while ((read = cryptoStream.Read(buffer, 0, buffer.Length)) > 0)
+                                {
+                                    fsOut.Write(buffer, 0, read);
+                                }
+                                txtPass.Clear();
+                                MessageBox.Show("File successfully decrypted!");
+                            }
+                            catch
+                            {
+                                MessageBox.Show("Wrong Password!");
+                                this.Close();
+                            }
+                        }
+                    }
+                    else if (cbFileType.SelectedIndex == 2)
+                    {
+                        using (FileStream fsOut = new FileStream(outputFile + ".rar", FileMode.Create))
+                        {
+                            try
+                            {
+                                int read;
+                                byte[] buffer = new byte[1048576];
+                                while ((read = cryptoStream.Read(buffer, 0, buffer.Length)) > 0)
+                                {
+                                    fsOut.Write(buffer, 0, read);
+                                }
+                                txtPass.Clear();
+                                MessageBox.Show("File successfully decrypted!");
+                            }
+                            catch
+                            {
+                                MessageBox.Show("Wrong Password!");
+                                this.Close();
+                            }
+                        }
+                    }
+                    else
+                    {
+                        using (FileStream fsOut = new FileStream(outputFile + ext, FileMode.Create))
+                        {
+                            try
+                            {
+                                int read;
+                                byte[] buffer = new byte[1048576];
+                                while ((read = cryptoStream.Read(buffer, 0, buffer.Length)) > 0)
+                                {
+                                    fsOut.Write(buffer, 0, read);
+                                }
+                                txtPass.Clear();
+                                MessageBox.Show("File successfully decrypted!");
+                            }
+                            catch
+                            {
+                                MessageBox.Show("Wrong Password!");
+                                this.Close();
+                            }
+                        }
+                    }
+                    
+                }
+            }
+        }
+        //Decrypt Button
+        private void btnDecrypt_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            saveFileDialog1.ShowDialog();
+            Decrypt(openFileDialog1.FileName, saveFileDialog1.FileName, txtPass.Text);
+        }
+    }
+}
